7ffe74f2becbb9604520a999a5fce15fe7e2f86d
lucaspineda <lucas.pineda@hotmail.com>
2020-10-16 19:01:08 -0300

    create navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <nav class="flex items-center justify-between flex-wrap bg-white p-6">
+      <div class="rounded-sm bg-gray-900">LP</div>
+    </nav>
     <Nuxt />
   </div>
 </template>
+<style scoped>
+
+div {
+  background-color: rgb(34, 34, 158);
+}
+
+nav {
+  background-color: rgb(242,234,230);
+}
+
+</style>
