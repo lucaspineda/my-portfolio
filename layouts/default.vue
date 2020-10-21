e287393125717c5f5f0a953b52d8042920dc9eff
Lucas Pineda <lucas.pineda@hotmail.com>
2020-10-20 23:57:33 -0300

    progress

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="bg-blue-900">
     <nav class="flex items-center justify-between flex-wrap bg-white p-6">
-      <div class="rounded-sm bg-gray-900">LP</div>
+      <div class="flex items-center justify-center rounded-full bg-blue-900 text-white name-avatar">LP</div>
     </nav>
     <Nuxt />
   </div>
@@ -9,11 +9,19 @@
 <style scoped>
 
 div {
-  background-color: rgb(34, 34, 158);
+  /* background-color: rgb(34, 34, 158); */
 }
 
 nav {
   background-color: rgb(242,234,230);
+}
+
+.name-avatar {
+  width: 4rem;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 </style>
