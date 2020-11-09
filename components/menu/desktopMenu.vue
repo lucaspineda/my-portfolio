5ef1ff94cc8b4357b761f4e9eb58e6e542576068
lucaspineda <lucas.pineda@hotmail.com>
2020-11-09 01:31:40 -0300

    finish desktop menu layout

---
+++ components/menu/desktopMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <aside class="desktop-menu fixed h-screen flex flex-col items-center text-center uppercase">
+    <img class="profile-image" src="@/assets/img/me.jpeg" alt="My profile picture">
+    <nav class="navigation-menu">
+      <ul class="navigation-menu__options-list">
+        <li>
+          About me
+        </li>
+        <li>
+          My projects
+        </li>
+        <li>
+          Contact me
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class aboutMe extends Vue {
+
+}
+</script>
+
+<style scoped>
+.desktop-menu {
+  background-color: var(--secondary-color);
+  width: 20rem;
+  color: var(--primary-color);
+}
+
+.profile-image {
+  height: 10rem;
+  width: 10rem;
+  border-radius: 100%;
+  margin: 6rem 0 8rem 0;
+}
+
+.navigation-menu__options-list {
+  font-size: 1.4rem;
+}
+
+.navigation-menu__options-list li{
+  margin: 0 0 .8rem 0;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,28 @@
 <template>
   <div>
-    <nav class="flex items-center fixed w-full">
-      <div class="flex items-center justify-center rounded-full text-white name-avatar">
-        LP
-      </div>
-      <div class="nav-icons flex items-center">
-        <svg width="30px" height="30px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-        </svg>
-        <svg width="34px" height="34px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-        </svg>
-        <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-        </svg>
-      </div>
-    </nav>
+    <DesktopMenu />
+    <MobileMenu />
     <Nuxt />
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  components: {
+    DesktopMenu: () => import('@/components/menu/desktopMenu.vue'),
+    MobileMenu: () => import('@/components/menu/mobileMenu.vue')
+  }
+})
+
+export default class defaultLayout extends Vue {
+
+}
+</script>
 <style scoped>
 
 .teste {
   /* background-color: var(--primary-color); */
-}
-
-nav {
-  background-color: var(--secondary-color);
-  padding: 1rem;
-}
-
-.name-avatar {
-  font-size: 1.6rem;
-  font-family: var(--body-font-family);
-  width: 4rem;
-  height: 4rem;
-  background-color: var(--primary-color);
-}
-
-.nav-icons {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-.nav-icons svg {
-  max-height: 100%;
-  max-width: 100%;
-  margin-left: 1rem;
-  color: var(--primary-color);
 }
 </style>
