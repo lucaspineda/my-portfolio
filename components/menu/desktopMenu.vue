4c87142415fdf379c15d6f167b3294ba7ff84a34
lucaspineda <lucas.pineda@hotmail.com>
2020-11-11 02:25:17 -0300

    create nav actions for mobile

--- components/menu/desktopMenu.vue
+++ components/menu/desktopMenu.vue
@@ -1,46 +1,22 @@
 <template>
-  <aside class="desktop-menu fixed h-screen flex flex-col items-center text-center uppercase">
+  <aside class="desktop-menu hidden fixed h-screen sm:flex flex-col items-center text-center uppercase">
     <img class="profile-image" src="@/assets/img/me.jpeg" alt="My profile picture">
     <nav class="navigation-menu">
       <ul class="navigation-menu__options-list">
-        <li>
-          <scrollactive class="my-nav" @itemchanged="onItemChanged">
-            <li>
-              <nuxt-link :to="{ path: '#about-me' }" replace class="scrollactive-item">Home</nuxt-link>
-            </li>
-            <li>
-              <a href="#my-projects" class="scrollactive-item">About Me</a>
-            </li>
-            <li>
-              <a href="#contact-me" class="scrollactive-item">Contact</a>
-            </li>
-          </scrollactive>
-        </li>
-        <!-- <li>
-          <nuxt-link to="#about-me">
-            teste
-          </nuxt-link>
-        </li>
-        <li>
-          <div v-scroll-to="{el: '#about-me'}">
-            Contact
-          </div>
-        </li>
-        <li>
-          <a href="#about-me">
-            About me
-          </a>
-        </li>
-        <li>
-          <div v-scroll-to="{el: '#my-projects', onStart: teste, onDone: teste2}">
-            My projects
-          </div>
-        </li>
-        <li>
-          <a href="#contact-me">
-            Contact me
-          </a>
-        </li> -->
+        <scrollactive class="my-nav" @itemchanged="onItemChanged">
+          <li>
+            <!-- <nuxt-link :to="{ path: '#about-me' }" class="scrollactive-item">About Me</nuxt-link> -->
+            <a href="#about-me" class="scrollactive-item">About Me</a>
+          </li>
+          <li>
+            <!-- <nuxt-link :to="{ path: '#my-projects' }" class="scrollactive-item">My Projects</nuxt-link> -->
+            <a href="#my-projects" class="scrollactive-item">My Projects</a>
+          </li>
+          <li>
+            <!-- <nuxt-link :to="{ path: '#contact-me' }" class="scrollactive-item">Contact</nuxt-link> -->
+            <a href="#contact-me" class="scrollactive-item">Contact</a>
+          </li>
+        </scrollactive>
       </ul>
     </nav>
   </aside>
@@ -53,12 +29,9 @@ import VueScrollactive from 'vue-scrollactive'
 Vue.use(VueScrollactive)
 
 @Component
-export default class aboutMe extends Vue {
+export default class DesktopMenu extends Vue {
   onItemChanged (...args) {
-    // here you have access to everything you need regarding that event
-    // console.log(currentItem.getAttribute('href'))
     const currentItemHref = args[1].getAttribute('href')
-    // console.log(args[1].getAttribute('href'))
     history.pushState(null, '', currentItemHref)
   }
 }
@@ -90,7 +63,7 @@ export default class aboutMe extends Vue {
   color: var(--senary-color);
 }
 
-.is-active {
+.is-active, .nuxt-link-exact-active {
   color: var(--primary-color);
 }
 </style>
